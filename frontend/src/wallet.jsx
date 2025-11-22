c507fece3579d2dbb58a2c4a84065a8d5fe22b55
arpradio <contact@psyencelab.media>
2025-11-22 13:36:32 -0700

    icp id authentication

--- frontend/src/wallet.jsx
+++
@@ -1,9 +0,0 @@
-function Wallet() {
-  return (
-    <div className="flex w-fit p-2 mt-2 items-center border-neutral-300 border hover:scale-105 hover:font-bold">
-   Wallet
-     </div>
-  );
-}
-
-export default Wallet;
